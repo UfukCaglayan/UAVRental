531406bae849987f9719368998fde38cb8b0a0c2
UfukCaglayan <ufukcaglayan96@gmail.com>
2023-08-15 00:56:55 +0300

    Update forms.py

--- DjangoAPI/UAVRental/forms.py
+++ DjangoAPI/UAVRental/forms.py
@@ -1,11 +1,7 @@
 from django import forms
-from django.forms import SelectMultiple, TextInput, Textarea
 
-from UAVRental.models import Rental
-
-class CourseCreateForm(forms.ModelForm):
-    class Meta:
-        model = Rental
-        fields = ('UavID','CustomerID','BeginDate','EndDate','Created')
+#Resim ekleme işleminin yapılabilmesini sağlayan form
+class UploadForm(forms.Form):
+    image = forms.ImageField()
 
    

580ee9891d63c03723ea31d989464e5f357ceda9
UfukCaglayan <ufukcaglayan96@gmail.com>
2023-08-15 00:55:33 +0300

    Update serializers.py

--- DjangoAPI/UAVRental/serializers.py
+++ DjangoAPI/UAVRental/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
-from UAVRental.models import Uavs,Rental
+from UAVRental.models import Uav,Rental,Brand,Model,Category
 
-class UavSerializer(serializers.ModelSerializer):
+class UavSerializer(serializers.UavSerializer):
     class Meta:
-        model = Uavs
-        fields = ('UavID','Brand','Model','Weight','Image','CategoryID')
+        model = Uav
+        fields = ('UavID','BrandID','ModelID','Weight','Image','CategoryID')
+
+class BrandSerializer(serializers.BrandSerializer):
+    class Meta:
+        model = Brand
+        fields = ('BrandID','BrandName')
+
+class ModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Model
+        fields = ('ModelID','BrandID','ModelName')
+
+class CategorySerializer(serializers.CategorySerializer):
+    class Meta:
+        model = Category
+        fields = ('CategoryID','CategoryName')
+
 
 class RentalSerializer(serializers.RentalSerializer):
     class Meta:

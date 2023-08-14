13151e63c96c0eca567964db3f7ece5794952801
UfukCaglayan <ufukcaglayan96@gmail.com>
2023-08-15 00:43:19 +0300

    Update urls.py

--- DjangoAPI/DjangoAPI/urls.py
+++ DjangoAPI/DjangoAPI/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,re_path
-
 from django.conf.urls import re_path,include
 from django.urls import re_path as url
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^',include('UAVRental.urls'))
 ]

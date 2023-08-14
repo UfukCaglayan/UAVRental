3488c080acf430ad82a764a14b85b17a88afb2d5
UfukCaglayan <ufukcaglayan96@gmail.com>
2023-08-15 00:33:22 +0300

    Update admin.py

--- DjangoAPI/UAVRental/admin.py
+++ DjangoAPI/UAVRental/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from UAVRental.models import Uavs
-import django
-# Register your models here.
+from UAVRental.models import Uav,Brand,Model,Category
 
+admin.site.register(Uav)
+admin.site.register(Brand)
+admin.site.register(Model)
+admin.site.register(Category)
 
-admin.site.register(Uavs)
+# Oluşturulan modellerin admin paneline eklenmesiyle beraber kayıt ekleme,silme ve güncelleme işlemler yapılabilmektedir.

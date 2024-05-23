92d89d7b38c1ca88b53041dc052048d8c4e4618b
UfukCaglayan <ufukcaglayan96@gmail.com>
2024-05-23 17:52:46 +0300

    Update forms.py
    Mail bilgisinin girilmesi sağlanmıştır

--- DjangoAPI/UAVRental/forms.py
+++ DjangoAPI/UAVRental/forms.py
@@ -1,7 +1,15 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 #Resim ekleme işleminin yapılabilmesini sağlayan form
 class UploadForm(forms.Form):
     image = forms.ImageField()
 
-   
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')

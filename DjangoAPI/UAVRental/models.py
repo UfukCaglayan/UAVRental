c3fb07744a08b95a36188e955740dc4b6d13ae92
UfukCaglayan <ufukcaglayan96@gmail.com>
2024-05-23 18:09:47 +0300

    Update models.py
    Modelin veritabanındaki değişikliklere uyumlu hale geitilmesi

--- DjangoAPI/UAVRental/models.py
+++ DjangoAPI/UAVRental/models.py
@@ -1,63 +1,67 @@
 from django.db import models
-from django.db.models import Q
-from UAVRental import forms
 from django.shortcuts import redirect
-from UAVRental.forms import UploadForm
+from UAVRental import forms
 
-#Veritabanındaki tablolara karşılık gelecek olan model classları yazılmıştır
+# Veritabanındaki tablolara karşılık gelecek olan model classları yazılmıştır
 
-#Marka bilgileri için model
+# Marka bilgileri için model
 class Brand(models.Model):
-    BrandID = models.AutoField(primary_key=True)
-    BrandName = models.CharField(max_length=50)
+    brand_id = models.AutoField(primary_key=True)
+    brand_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.BrandName
+        return self.brand_name
 
-#İHA modelleri için model (modeller markalara bağlıdır)
+
+# İHA modelleri için model (modeller markalara bağlıdır)
 class Model(models.Model):
-    ModelID = models.AutoField(primary_key=True)
-    BrandID = models.ForeignKey(Brand,on_delete=models.CASCADE)
-    ModelName = models.CharField(max_length=50)
+    model_id = models.AutoField(primary_key=True)
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    model_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.ModelName
-    
-#Kategori bilgileri için model
+        return self.model_name
+
+
+# Kategori bilgileri için model
 class Category(models.Model):
-    CategoryID = models.AutoField(primary_key=True)
-    CategoryName = models.CharField(max_length=50)
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.CategoryName
-    
-#Kategori bilgileri için model
+        return self.category_name
+
+
+# İHA bilgileri için model
 class Uav(models.Model):
-    UavID = models.AutoField(primary_key=True)
-    BrandID = models.ForeignKey(Brand,on_delete=models.CASCADE)
-    ModelID = models.ForeignKey(Model,on_delete=models.CASCADE)
-    Weight = models.IntegerField()
-    Image = models.ImageField(upload_to="images",default="")
-    CategoryID = models.ForeignKey(Category,on_delete=models.CASCADE)
+    uav_id = models.AutoField(primary_key=True)
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    model_id = models.ForeignKey(Model, on_delete=models.CASCADE)
+    weight = models.IntegerField()
+    image = models.ImageField(upload_to="images", default="")
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
 
- #Kiralama kayıtları için model
+
+# Kiralama kayıtları için model
 class Rental(models.Model):
-    RentalID = models.AutoField(primary_key=True)
-    UavID =  models.ForeignKey(Uav,on_delete=models.CASCADE)
-    CustomerID = models.IntegerField()
-    BeginDate = models.DateTimeField()
-    EndDate = models.DateTimeField()
-    Created = models.DateTimeField(auto_now_add=True)
+    rental_id = models.AutoField(primary_key=True)
+    uav_id = models.ForeignKey(Uav, on_delete=models.CASCADE)
+    customer_id = models.IntegerField()
+    begin_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
 
-#İHA resimlerinin yüklenmesi
+
+# İHA resimlerinin yüklenmesi
 def upload(request):
     if request.method == "POST":
-        form = UploadForm(request.POST,request.FILES)
+        form = forms.UploadForm(request.POST, request.FILES)
 
-        if(form.is_valid()):
-            model = UploadModel(image = request.FILES["image"])
+        if form.is_valid():
+            model = UploadModel(image=request.FILES["image"])
             model.save()
             return redirect('/')
+
 
 class UploadModel(models.Model):
     image = models.ImageField(upload_to="images")

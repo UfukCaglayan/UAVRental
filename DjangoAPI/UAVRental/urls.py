9b11d9ea17d52f2e715e3b521bbc84d90bfebf4d
UfukCaglayan <ufukcaglayan96@gmail.com>
2024-05-23 17:51:53 +0300

    Update urls.py
    Django yeni sürüme göre güncellemeler yapılmıştır

--- DjangoAPI/UAVRental/urls.py
+++ DjangoAPI/UAVRental/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path, include
 from UAVRental import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -8,18 +7,19 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name='index'),
     path('index/', views.index, name='index'),
-    path('signin/',views.signin),
-    path('signout/',views.signout),
-    path('signup/',views.signup),
-    path('profile/',views.profile),
-    path('rental/<int:id>', views.rental,name="rental"),
-    path('myrental/<int:id>', views.myrental, name='myrental'),
-    path('rentaledit/<int:id>', views.editRental,name="editRental"),
-    path('confirmDelete/<int:id>', views.confirmDelete,name="confirmDelete"),
-    url('rental/createRental$', views.createRental, name='createRental'),
-    url('rentaledit/updateRental$', views.updateRental, name='updateRental'),
-    url('confirmDelete/deleteRental$', views.deleteRental, name='deleteRental'),
-] 
+    path('signin/', views.signin),
+    path('signout/', views.signout),
+    path('signup/', views.signup),
+    path('profile/', views.profile),
+    path('rental/<int:id>/', views.rental, name="rental"),
+    path('myrental/<int:id>/', views.myrental, name='myrental'),
+    path('rentaledit/<int:id>/', views.editRental, name="editRental"),
+    path('confirmDelete/<int:id>/', views.confirmDelete, name="confirmDelete"),
+    path('rental/createRental/', views.createRental, name='createRental'),  # Güncellendi
+    path('rentaledit/updateRental/', views.updateRental, name='updateRental'),
+    path('confirmDelete/deleteRental/', views.deleteRental, name='deleteRental'),
+]
+
 # Sayfalar arası yönlendirme işlemlerinin yapılabilmesi için gerekli kodlar yazılmıştır. 
 # Viewsin içinde bulunan ve sayfalar için kullanılan metodlar aktif hale getirilmektedir.
 
@@ -27,4 +27,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Resme tıklandığında açılması için gerekli maskeleme işlemi yapılmıştır.
-

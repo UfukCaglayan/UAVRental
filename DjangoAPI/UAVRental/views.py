cdf15c397899f7cfaf254b8a4790f1ace564fc56
UfukCaglayan <ufukcaglayan96@gmail.com>
2024-05-23 17:50:23 +0300

    Update views.py
    Mail gönderimi ve raporlanmaya dair kuyruklama işlemleri eklenmiştir

--- DjangoAPI/UAVRental/views.py
+++ DjangoAPI/UAVRental/views.py
@@ -10,6 +10,21 @@ from .models import Uav,Rental,Brand,Model,Category
 from django.views.decorators.csrf import csrf_exempt
 from django.forms import formset_factory
 from .forms import *
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
+from datetime import datetime
+import pytz
+import pika
+from django.utils.dateparse import parse_datetime
+from .tasks import send_email_task
+from django.utils import timezone
+from datetime import datetime
+import os
+import pandas as pd
+from django.conf import settings
+from openpyxl import load_workbook
+
 #Site ilk açıldığında mevcut ihaların listelenmesi
 def index(request):
     uav_list = Uav.objects.order_by('UavID') 
@@ -29,12 +44,15 @@ def index(request):
             uav_list = Uav.objects.filter(BrandID__in = brand_ids) | Uav.objects.filter(ModelID__in = model_ids) | Uav.objects.filter(CategoryID__in = category_ids)
                         #Filtreleme yapılarak id'si, tıklanan markaların id'si olan İHA'lar getiriliyor. Bu işlem model ve kategori içinde yapılıyor.
       
+
     context = {
         'uav_list': uav_list,
         'brand_list': brand_list,
         'model_list': model_list,
         'category_list': category_list,
     }
+  
+
     return render(request, 'index.html', context)
 
   
@@ -45,7 +63,7 @@ def signup(request):
     if request.user.is_authenticated:
         return redirect('/')
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -56,7 +74,7 @@ def signup(request):
         else:
             return render(request, 'signup.html', {'form': form})
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
         return render(request, 'signup.html', {'form': form})
     
 
@@ -95,11 +113,38 @@ def rental(request, id):
 
 #Formdan İHA,müşteri ve tarih-zaman bilgilerinin alınmasıyla beraber kiralama işlemi yapılmaktadır ve mevcut kiralama
 #bilgilerinin listelendiği sayfaya yönlendirme işlemi yapılmıştır.
-@csrf_exempt
+
 def createRental(request):
-    rental = Rental(UavID= Uav.objects.get(UavID =  request.POST['UavID']) ,CustomerID=request.POST['CustomerID'],BeginDate=request.POST['BeginDate'], EndDate=request.POST['EndDate'])
-    rental.save()
+    if request.method == 'POST':
+        # datetime stringlerini datetime objelerine dönüştürme
+        begin_date = datetime.strptime(request.POST['BeginDate'], '%Y-%m-%dT%H:%M')
+        end_date = datetime.strptime(request.POST['EndDate'], '%Y-%m-%dT%H:%M')
+
+        # datetime objelerini timezone-aware yapma
+        begin_date = timezone.make_aware(begin_date, timezone.get_current_timezone())
+        end_date = timezone.make_aware(end_date, timezone.get_current_timezone())
+
+        rental = Rental(
+            UavID=Uav.objects.get(UavID=request.POST['UavID']),
+            CustomerID=request.POST['CustomerID'],
+            BeginDate=begin_date,
+            EndDate=end_date
+        )
+        rental.save()
+        
+        save_to_excel(rental,request)
+        send_email_task.delay('Kiralama Onayı', 'Kiralama işleminiz başarıyla tamamlanmıştır', ['ufukcaglayan96@gmail.com']) #kullanıcıya mail gönderme işlemi kuyruğa alınmıştır
     return redirect('/myrental/' + request.POST['CustomerID'])
+
+
+def save_to_excel(rental, request):
+    username = request.user.username
+    uav = get_object_or_404(Uav, pk=rental.UavID.UavID)
+    brand_model = uav.BrandID.BrandName + ' ' + uav.ModelID.ModelName
+    begin_date = rental.BeginDate.replace(tzinfo=None)
+    end_date = rental.EndDate.replace(tzinfo=None)
+    save_to_excel.delay(username,brand_model,begin_date,end_date) #raporlama işlemi kuyruğa alınmıştır
+      
 
 #Kiralama kayıtlarının gösterilmesi
 @csrf_exempt
